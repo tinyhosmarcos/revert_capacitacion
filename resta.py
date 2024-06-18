e6660b0b5f8c705c99009c8746cae3966bcd304e
Yhostin Marcos Ollachica Arias <yhostin.ollachica@ucsp.edu.pe>
2024-06-18 11:53:00 -0500

    add archivos

--- resta.py
+++ resta.py
@@ -3,7 +3,8 @@
 def resta(A,B):
     C = []
     for i in range(len(A)):
-        C.append([])
         for j in range(len(A[0])):
             C[i].append(A[i][j] - B[i][j])
     return C
+
+    #cambio 2
