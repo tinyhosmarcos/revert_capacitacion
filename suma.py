30194bbeca07b686b108ed894cf7de387b0bfa4c
Yhostin Marcos Ollachica Arias <yhostin.ollachica@ucsp.edu.pe>
2024-06-18 10:50:07 -0500

    add: se añadio el arcchivo de suma

---
+++ suma.py
@@ -0,0 +1,12 @@
+# funcion de suma de matrices
+
+def suma(A,B):
+    C = []
+    for i in range(len(A)):
+        C.append([])
+        for j in range(len(A[0])):
+            C[i].append(A[i][j] + B[i][j])
+    return C
+
+    
+
